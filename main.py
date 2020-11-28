9fd28e174ada3efd58686fb8ad08dc1271ca35de
cassis163 <cas.aangeenbrug@gmail.com>
2020-11-28 18:19:37 +0100

    Add steps in main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import scraper
-import text_fitter
+import formatter
 from datetime import date
 from os import getcwd
+from subprocess import Popen
 
 file_name = date.today().strftime('%Y-%m-%d')
 file_dir = getcwd() + '/excel/' + file_name + '.xlsx'
 
 df = scraper.get_dataframe()
-df.to_excel(file_dir, index=None)
 
-text_fitter.fit(file_dir)
+df_out = df.drop(columns=['Soort'])
+df_out.to_excel(file_dir, index=None)
+
+formatter.wrap_text(file_dir)
+formatter.add_headers(file_dir, df['Soort'])
+
+# Open the Excel file in Microsoft Excel
+Popen(file_dir)
